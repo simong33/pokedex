d5cb2fc9f284ac604ffe2eb39e56ba9f0bbd5e34
simon guillorit <simon.guillorit@gmail.com>
2018-02-17 11:52:07 +0100

    Fetch name and sprite of pokémon

--- src/App.js
+++ src/App.js
@@ -1,18 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      image: "",
+      search: ""
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  handleSearch = (e) => {
+    this.setState({
+      search: e.target.value,
+    })
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios.get(`http://pokeapi.co/api/v2/pokemon/${this.state.search}`)
+      .then(response => {
+        let data = response.data;
+
+        this.setState({
+          name: data.name,
+          image: data.sprites.front_default
+        })
+      })
+      .catch(error => console.log(error));
+
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Espèce: {this.state.name}</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img className="picture" url={this.state.image} alt={this.state.pokemon} />
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Search Pokémon"
+            value={this.state.search}
+            onChange={this.handleSearch}
+          />
+          <button
+            className="button"
+            type="submit"
+          >
+            Find
+          </button>
+        </form>
       </div>
     );
   }
