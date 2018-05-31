ad817b0050e51161f0566204619d3a168f9a426c
Simon Guillorit <simon.guillorit@gmail.com>
2018-05-31 11:50:43 +0200

    Add rotate effect when following pointer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,83 @@ import Search from './components/Search';
 import Screen from './components/Screen';
 import Loading from './components/Loading';
 
+const rotateAnimation = () => {
+  var container = document.getElementById("container"),
+      inner = document.getElementById("gameboy");
+
+    // Mouse
+    var mouse = {
+      _x: 0,
+      _y: 0,
+      x: 0,
+      y: 0,
+      updatePosition: function(event) {
+        var e = event || window.event;
+        this.x = e.clientX - this._x;
+        this.y = (e.clientY - this._y) * -1;
+      },
+      setOrigin: function(e) {
+        this._x = e.offsetLeft + Math.floor(e.offsetWidth / 2);
+        this._y = e.offsetTop + Math.floor(e.offsetHeight / 2);
+      },
+      show: function() {
+        return "(" + this.x + ", " + this.y + ")";
+      }
+    };
+
+    // Track the mouse position relative to the center of the container.
+    mouse.setOrigin(container);
+
+    //-----------------------------------------
+
+    var counter = 0;
+    var updateRate = 10;
+    var isTimeToUpdate = function() {
+      return counter++ % updateRate === 0;
+    };
+
+    //-----------------------------------------
+
+    var onMouseEnterHandler = function(event) {
+      update(event);
+    };
+
+    var onMouseLeaveHandler = function() {
+      inner.style = "";
+    };
+
+    var onMouseMoveHandler = function(event) {
+      if (isTimeToUpdate()) {
+        update(event);
+      }
+    };
+
+    //-----------------------------------------
+
+    var update = function(event) {
+      mouse.updatePosition(event);
+      updateTransformStyle(
+        (mouse.y / inner.offsetHeight / 2).toFixed(2),
+        (mouse.x / inner.offsetWidth / 2).toFixed(2)
+      );
+    };
+
+    var updateTransformStyle = function(x, y) {
+      var style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
+      inner.style.transform = style;
+      inner.style.webkitTransform = style;
+      inner.style.mozTransform = style;
+      inner.style.msTransform = style;
+      inner.style.oTransform = style;
+    };
+
+    //-----------------------------------------
+
+    container.onmouseenter = onMouseEnterHandler;
+    container.onmouseleave = onMouseLeaveHandler;
+    container.onmousemove = onMouseMoveHandler;
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -19,6 +96,7 @@ class App extends Component {
 
   componentDidMount() {
     document.title="Pokédex"
+    rotateAnimation()
   }
 
   onSearch = (data) => {
@@ -29,7 +107,10 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div
+        className="App"
+        id="container"
+        >
         <Gameboy>
           <Search onSearch={this.onSearch}/>
           {!this.state.pokemon
